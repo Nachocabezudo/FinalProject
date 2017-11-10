2a1c4909b5dd3db9ece7be1ee732aa3ea578afbe
unknown <nachocab@csde-ts2.csde.washington.edu>
2017-11-10 09:15:28 -0800

    Backing up for server reset

--- ACLEDsort.R
+++ ACLEDsort.R
@@ -1,24 +1,23 @@
 # Calling packages
 
 library(ggplot2)
-library(tidyverse)
+library(dplyr)
 library(WDI)
 
 
 #Import the Dataset
 ACLED <- read_csv("ACLED-AFRICA-FULL.csv")
 
-#The next step will pull the event data and seperate them into groups by country
-#First step: Generate a list of all countries represented in the data set
-
+#Generate a list of countries that the data applies to
 countries <- unique(ACLED[,15])
 
-#Second Step: filter the data into sets representing each country we are looking at
-#for the purposes of this exercises we'll look at the countries known to be effected 
-#by Boko Haram
+#Filter options
 
-#Filter out events that had fatalities
-fatal_ACLED <- dplyr::filter(ACLED, FATALITIES > 0)
+#Pull the information that we are most concerned with
+
+ACLED_parsed <- ACLED %>% 
+  select(EVENT_DATE, COUNTRY, YEAR, FATALITIES, LOCATION, LONGITUDE, LATITUDE) %>%
+  dplyr::filter(FATALITIES > 0) #Filter out the non-fatal events
 
 #Filter by country
 Chad <- filter(fatal_ACLED, COUNTRY == "Chad")
@@ -26,8 +25,6 @@ Niger <- filter(fatal_ACLED, COUNTRY == "Niger")
 Nigeria <- filter(fatal_ACLED, COUNTRY == "Nigeria")
 Cameroon <- filter(fatal_ACLED, COUNTRY == "Cameroon")
 
-
-#Filtering out fatalities
 
 
 
